a2ce29a7167f22f3c82fb1162b110014ba2656a7
Chaitanya-2305 <annaluruchaitanya@gmail.com>
2025-07-28 01:40:46 +0530

    🔧 Updated urls

--- Eco_Trio_Main/urls.py
+++ Eco_Trio_Main/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from Eco_Trio_Sub import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Eco_Trio_Sub.urls')),
+    path('', views.home_view, name='home'),  # This handles the root URL "/"
+    # Add other paths as needed
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

bc649218a6da3577b35f1d4d15d31f59b470316b
Chaitanya-2305 <annaluruchaitanya@gmail.com>
2025-07-28 01:56:46 +0530

    Added static video file and updated HTML template

--- Eco_Trio_Main/urls.py
+++ Eco_Trio_Main/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from Eco_Trio_Sub import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_view, name='home'),  # This handles the root URL "/"
     # Add other paths as needed
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

1d845d60060511009dc35f130089432452f6cfdd
Chaitanya-2305 <annaluruchaitanya@gmail.com>
2025-07-27 16:05:51 +0530

    Initial Commit

--- Eco_Trio_Sub/urls.py
+++ Eco_Trio_Sub/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.shortcuts import redirect
 from .views import home_view, agri_services_view, signup_view, signin_view,send_otp,logout_view,register_view,submit_form,global_view, digital_grow, team_view,contact_view,collab_view,apply_view,subscribe_view  # Import your views here
  # Make sure to import your actual view function
 urlpatterns = [
-    path('', home_view, name='home'),  # ✅ Use the function, not a string
+    path('', lambda request: redirect('home')),  # 👈 Redirect root to /home
+    path('home/', home_view, name='home'),
     #path('send-otp/', send_otp, name='send_otp'),
     path('register/',register_view, name='register'),  # ✅ Use the function, not a string
     path('send-otp-email/', send_otp, name='send_otp_email'),

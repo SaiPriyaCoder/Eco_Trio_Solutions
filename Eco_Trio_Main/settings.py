1318add827bd1dce3d57ab2abb2cb82443107e35
Chaitanya-2305 <annaluruchaitanya@gmail.com>
2025-07-27 17:20:40 +0530

    Fix broken requirements update final

--- Eco_Trio_Main/settings.py
+++ Eco_Trio_Main/settings.py
@@ -78,12 +78,14 @@ USE_I18N = True
 USE_TZ = True
 
 # ✅ Static and media files
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+#     os.path.join(BASE_DIR, 'Eco_Trio_Main', 'statics'),
+# ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'Eco_Trio_Main', 'statics'),
-]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
